f281a392366b590f0f9e7e013aa59e7d92f24480
Jeddi212 <jedediahf51@gmail.com>
2021-12-21 23:09:56 +0700

    User Input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::{stdin, stdout, Write};
+
+fn read(input: &mut String) {
+    stdout().flush().expect("Failed to flush");
+    stdin().read_line(input).expect("Failed to read input");
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut input = String::new();
+
+    welcoming();
+    read(&mut input);
+
+    println!("{}", input);
+}
+
+fn welcoming() {
+    println!("Rock Paper Scissor Game!!!");
+    print!("Please choose your weapon
+1. Rock
+2. Paper
+3. Scissor
+4. Exit
+-> ");
 }
